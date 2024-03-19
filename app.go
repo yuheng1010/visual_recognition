fd6390165ad36d55aa0c4fdd4755d5ec482f6ca5
Yuheng Hung <heng@YuhengdeMacBook-Pro.local>
2024-03-19 17:26:44 +0800

    03192

---
+++ app.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+	"visual_recognition/server/routes"
+)
+
+func main() {
+	r := routes.NewRouter()
+	http.ListenAndServe(":8080", r)
+}
---
+++ server/controllers/level_controller.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func AllMovies(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented !")
+}
+
+func FindMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented !")
+}
+
+func CreateMovie(w http.ResponseWriter, t *http.Request) {
+	fmt.Fprintln(w, "not implemented !")
+}
+
+func UpdateMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented !")
+}
+
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented !")
+}
